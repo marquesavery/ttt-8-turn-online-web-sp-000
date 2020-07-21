519d5fed0fb8ed2dba58198c402916aef9fdabeb
Marques Avery <averymarques4@gmail.com>
2020-07-21 21:40:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,7 +29,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   index = input_to_index(gets.strip)
-  until index.to_i <= 8 || index.to_i >= 1
+  until index.to_i <= 9 || index.to_i >= 1
     if valid_move?(board, index) == true
       move(board, index)
     end
