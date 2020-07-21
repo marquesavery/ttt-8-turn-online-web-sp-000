c0ecb94cc0326e6807e06b237040026a9c921627
Marques Avery <averymarques4@gmail.com>
2020-07-21 20:53:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def valid_move?(board, index)
     false
   elsif board[index] == " " || board[index] == "" || board[index] == nil
     true
-  else board[index] > 8 || board[index] < 0
+  else index > 8 || index < 0
     false
   end
 end
